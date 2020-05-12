627ac2346bf8742b6a8e226eceaaede93c48fe06
ThomasPinglot <55755788+ThomasPinglot@users.noreply.github.com>
2020-05-12 12:34:08 +0200

    Add files via upload

--- MID TERM SOUTIEN/midterm-2-skeleton_and_subject/midterm-2/Cryptanalysis/Vigenere.cs
+++ MID TERM SOUTIEN/midterm-2-skeleton_and_subject/midterm-2/Cryptanalysis/Vigenere.cs
@@ -5,19 +5,48 @@ namespace Cryptanalysis
 public class Vigenere
 {
     public const float FrenchIndexOfCoincidence = 0.0778F;
+    private string Key;
     public Vigenere(string key)
     {
-        throw new NotImplementedException();
+        this.Key = key;
     }
 
     public string Encrypt(string msg)
     {
-        throw new NotImplementedException();
+        string a = "";
+        int count = 0;
+        int len = Key.Length;
+        foreach (char c in msg)
+        {
+            if (count == len)
+            {
+                count = 0;
+            }
+            a += Tools.RotChar(c, Key[count]);
+            count += 1;
+
+        }
+
+        return a;
     }
 
     public string Decrypt(string cypherText)
     {
-        throw new NotImplementedException();
+        string a = "";
+        int count = 0;
+        int len = Key.Length;
+        foreach (char c in cypherText)
+        {
+            if (count == len)
+            {
+                count = 0;
+            }
+            a += Tools.RotChar(c, -Key[count]);
+            count += 1;
+
+        }
+
+        return a;
     }
 
     public static string GuessKeyWithLength(string cypherText, int keyLength)
